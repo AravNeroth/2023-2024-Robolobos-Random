f9e5625c7cd947dbb19a771cce27fd3a92b7bb8d
AravNeroth <reacharav@outlook.com>
2023-09-08 22:21:01 -0500

    (UNTESTED) Timed Test 1
    timeTest() should run the motor for a small time and return the time taken.
    
    The speed should also change based on if the battery is > / < 13.5V

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LynxVoltageTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LynxVoltageTester.java
@@ -4,6 +4,7 @@ import com.qualcomm.hardware.lynx.LynxVoltageSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.CRServoImplEx;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -12,12 +13,10 @@ import com.acmerobotics.dashboard.*;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.*;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Func;
-import org.firstinspires.ftc.teamcode.subsystems.Arm;
-import org.firstinspires.ftc.teamcode.subsystems.Claw;
 import org.firstinspires.ftc.teamcode.subsystems.ControllerFeatures;
-import org.firstinspires.ftc.teamcode.subsystems.Turrent;
 
 /*
 Created by Arav Neroth on 9/8/2023, for FTC Teams 14361 & 14363
@@ -32,13 +31,19 @@ Drive Constant variables during Auton so that it is more consistant
 public class LynxVoltageTester extends LinearOpMode{
 
     // this changes the speed multiplier for wheels
-    double mult = 0.75;
+    double mult = 0.5;
+    long timeTaken;
+
+    ElapsedTime clock;
     LynxVoltageSensor voltageReader;
 
+    DcMotorEx motor4;
     @Override
     public void runOpMode() throws InterruptedException {
 
-        DcMotor motor4 = hardwareMap.dcMotor.get("motor4");
+        DcMotorEx motor4 = (DcMotorEx) hardwareMap.dcMotor.get("motor4");
+        motor4.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+
 
 // ------------- Below is the telementry for voltage ---------------- \\
 
@@ -70,15 +75,21 @@ public class LynxVoltageTester extends LinearOpMode{
 
         ControllerFeatures feature = new ControllerFeatures();
 
+        ElapsedTime clock = new ElapsedTime();
+
         // rumbles on initialization
         feature.rumbleOnStart(gamepad1, gamepad2);
         feature.setPink(gamepad1, gamepad2, 10);
+
+        clock.reset();
 
         waitForStart();
 
         if (isStopRequested()) return;
 
         while(opModeIsActive()) {
+
+            // ------------- Below is the telementry for voltage ---------------- \\
 
             telemetry.addData("Current Voltage Levels: ", "%.1f volts.", new Func<Double>() {
                 public Double value() {
@@ -87,30 +98,27 @@ public class LynxVoltageTester extends LinearOpMode{
             });
             telemetry.update();
 
-            // sets controller colors- find in Subsystem ControllerLights
-            feature.setRainbow(gamepad1, gamepad2);
+            // ----------- Above is the telementry for voltage ---------------- \\
+
+            if(getBatteryVoltage() > 13.5){
+                mult = 1;
+            } else if (getBatteryVoltage() < 13.5) {
+                mult = 0.25;
+            }
 
             // imu reset is dpad up
             if (gamepad1.dpad_up) {
                 imu.initialize(parameters);
             }
 
-            if(gamepad1.left_trigger > 0.8){
-                mult = 1;
-                feature.lightRumble(gamepad1, gamepad2, 500);
+
+            telemetry.addData("Motor4 Velocity: ", motor4.getVelocity());
+            telemetry.update();
+
+            if(gamepad1.x) {
+                telemetry.addData("Time Taken to Complete: ", timeTest());
+                telemetry.update();
             }
-
-            else if(gamepad1.right_trigger > 0.8){
-                mult = 0.5;
-                feature.lightRumble(gamepad1, gamepad2, 500);
-            }
-
-            else{
-                mult = 0.75;
-            }
-
-
-            motor4.setPower(mult * 1);
 
         }
 
@@ -125,6 +133,16 @@ public class LynxVoltageTester extends LinearOpMode{
             }
         }
         return result;
+    }
+
+    public long timeTest(){
+
+        motor4.setTargetPosition(500);
+        clock.reset();
+        motor4.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor4.setVelocity(mult);
+
+        return timeTaken;
     }
 
 }
