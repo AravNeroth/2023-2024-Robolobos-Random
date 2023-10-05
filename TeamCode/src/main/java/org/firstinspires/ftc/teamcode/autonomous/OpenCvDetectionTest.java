64b4a97da0f41221ef8ba08b26f5af6c990b7213
AravNeroth <reacharav@outlook.com>
2023-10-05 15:42:55 -0500

    goofed around

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/OpenCvDetectionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/OpenCvDetectionTest.java
@@ -1,4 +1,41 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
-public class OpenCvDetectionTest {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Detection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+
+@Autonomous(name="Skystone Detecotor", group="Auto")
+public class OpenCvDetectionTest extends LinearOpMode {
+    OpenCvCamera phoneCam;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        int cameraMonitorViewId = hardwareMap.appContext
+                .getResources().getIdentifier("cameraMonitorViewId",
+                        "id", hardwareMap.appContext.getPackageName());
+        phoneCam = OpenCvCameraFactory.getInstance()
+                .createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
+        Detection detector = new Detection(telemetry);
+        phoneCam.setPipeline(detector);
+        phoneCam.openCameraDeviceAsync(
+                () -> phoneCam.openCameraDeviceAsync(() -> phoneCam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT));
+        );
+
+        waitForStart();
+        switch (detector.getLocation()) {
+            case LEFT:
+                // ...
+                break;
+            case RIGHT:
+                // ...
+                break;
+            case NOT_FOUND:
+                // ...
+        }
+        phoneCam.stopStreaming();
+    }
 }
